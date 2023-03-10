da25b3a9df3296587ce9083672a465947fbf7825
Mads Stoumann <stoumann@gmail.com>
2023-03-10 14:36:15 +0100

    App updates

--- app.js
+++ app.js
@@ -1,54 +1,106 @@
+/**
+ * @class
+ * @classdesc App, sets CSS Custom Properties from form inputs
+ * @version 1.1.00
+ * @summary 03-10-2022
+ * @author Mads Stoumann
+ * @param {Node} app
+ * @param {Object} settings
+ */
 export default class App {
-	constructor(app, list, patterns, microNode) {
-		if (app) {
-			this.microNode = microNode
-			this.storage = true
-			this.init(app, list, patterns)
-		}
+	constructor(app, settings = {}) {
+		if (!app) return;
+		this.settings = Object.assign({
+			classNode: '',
+			listNode: '',
+			presets: [],
+			storage: ''
+		}, settings) 
+		this.init(app)
 	}
+	/**
+	 * @function add
+	 * @description Adds a preset to the list of presets
+	 * @param {Node} list
+	 * @param {String} name
+	 * @param {String|Object} data
+	 */
 	add(list, name, data) {
-		list.insertAdjacentHTML('beforeend', `<button type="button" data-preset='${typeof data === 'object' ? JSON.stringify(data): data}'>${name}</button>`)
+		const option = document.createElement('option')
+		option.value = typeof data === 'object' ? JSON.stringify(data): data
+		option.textContent =  name.replaceAll('-', ' ').replace(this.settings.storage, '').trim()
+		list.appendChild(option)
 	}
-	init(app, list, patterns) {
+	/**
+	 * @function classes
+	 * @description Adds/removes a micro-class to `classNode` based on the value of the radio input
+	 * @param {Node} input
+	 */
+	classes(input) {
+		const prefix = input.name + '-';
+		this.settings.classNode.className = this.settings.classNode.className.replace(new RegExp(`${prefix}[0-9]+`, ''), '');
+		if (input.value !== '0') this.settings.classNode.classList.add(prefix+input.value)
+	}
+	/**
+	 * @function init
+	 * @description Initializes the app
+	 * @param {Node} app
+	 */
+	init(app) {
 		app.addEventListener('input', event => { this.set(event.target) })
-		app.addEventListener('reset', event => { this.update(app) })
-		if (list) this.presets(app, list, patterns)
+		app.addEventListener('reset', (event) => { setTimeout(() => this.update(app), 50) })
+		if (this.settings.listNode) this.list(app, this.settings.listNode, this.settings.presets)
 		if (app.elements.save) { 
 			app.elements.save.addEventListener('click', () => {
 				const data = this.save(app)
-				console.log(`%cYour JSON is ready:\n%c${data}`, 'font-weight:700', 'padding:1ch')
-				if (this.storage) {
+				console.log(`%cJSON:\n%c${data}`, 'font-weight:700', 'padding:1ch')
+				if (this.settings.storage) {
 					const name = prompt('Save?') || 'Preset';
-					localStorage.setItem(name, data)
+					localStorage.setItem((this.settings.storage || 'app') + '-' + name.replaceAll(' ', '-') , data)
 					this.add(list, name, data)
-				} else {
-					alert('localStorage is not allowed. Check the console instead.')
 				}
 			})
 		}
 		this.update(app)
 	}
+	/**
+	 * @function load
+	 * @description Loads a preset
+	 * @param {Node} app
+	 * @param {Object} data
+	 */
 	load(app, data) {
 		Object.keys(data).forEach(key => app.elements[key].value = data[key])
 		this.update(app)
 	}
-	micro(input) {
-		if (this.microNode) {
-			const prefix = input.name + '-';
-			this.microNode.className = this.microNode.className.replace(new RegExp(`${prefix}[0-9]+`, ''), '');
-			if (input.value !== '0') this.microNode.classList.add(prefix+input.value)
-		}
-	}
-	presets(app, list, patterns = []) {
-		list.addEventListener('click', event => {
-			if (event.target.hasAttribute('data-preset')) this.load(app, JSON.parse(event.target.dataset.preset))
+	/**
+	 * @function list
+	 * @description Adds presets to list, creates eventListener
+	 * @param {Node} app 
+	 * @param {Node} list 
+	 * @param {Array} presets 
+	 */
+	list(app, list, presets) {
+		list.addEventListener('change', () => this.load(app, JSON.parse(list.value)))
+		Object.keys(presets).forEach(key => this.add(list, presets[key].name, presets[key].data))
+		if (this.settings.storage) Object.keys(localStorage).forEach(key => {
+			if (key.startsWith(this.settings.storage)) this.add(list, key, localStorage.getItem(key))
 		})
-		Object.keys(patterns).forEach(key => this.add(list, patterns[key].name, patterns[key].data))
-		if (this.storage) Object.keys(localStorage).forEach(key => this.add(list, key, localStorage.getItem(key)))
 	}
+	/**
+	 * @function save
+	 * @description Saves the current state of the app
+	 * @param {Node} app
+	 */
 	save(app) {
 		return JSON.stringify(Object.fromEntries(new FormData(app)))
 	}
+	/**
+	 * @function scope
+	 * @description Returns a node based on scope
+	 * @param {Node} input
+	 * @param {String} scope
+	 */
 	scope(input, scope) {
 		switch (scope) {
 			case 'fieldset': return input.closest('fieldset')
@@ -60,8 +112,13 @@ export default class App {
 			default: return scope?.length ? document.querySelector(scope) : input
 		}
 	}
+	/**
+	 * @function set
+	 * @description Sets a CSS Custom Property
+	 * @param {Node} input
+	 */
 	set(input) {
-		if (input?.type === 'radio') return this.micro(input)
+		if (this.settings.classNode && input?.type === 'radio') return this.classes(input)
 		const key = input.dataset.key || input.name
 		const node = this.scope(input, input.dataset.scope)
 		if (key && node) { 
@@ -69,9 +126,13 @@ export default class App {
 			node.style.setProperty(key.startsWith('--') ? key : '--' + key, value)
 		}
 	}
+	/**
+	 * @function update
+	 * @description Updates the app
+	 * @param {Node} app
+	 */
 	update(app) {
-		if (this.microNode) return
-		console.log('TODO:RESET')
+		if (this.settings.classNode) return
 		Array.from(app.elements).forEach(input => input.value && input.dispatchEvent(new Event('input', { bubbles: true })))
 	}
 }
